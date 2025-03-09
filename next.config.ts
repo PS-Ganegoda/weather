ae6063e76ddfb4e6943145a76e94852d42cfd7a2
GRPS GANEGODA <piumisarangag@gmail.com>
2025-03-09 11:25:46 +0530

    cchange the git file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enables static export when running `next build`
 };
 
 export default nextConfig;
